c27904c1adacd88236bc23e9b9cff2df2b415f11
elopezt <00014718@uca.edu.sv>
2020-02-27 14:56:33 -0600

    New commit

---
+++ src/components/Splash.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { withRouter } from "react-router-dom"
+
+class Splash extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: true,
+        }
+    }
+
+    async componentDidMount() {
+        const token = localStorage.getItem("token");
+
+
+        if (token) {
+            this.verifyToken(token);
+        }
+        else {
+            this.props.history.push("/login")
+        }
+    }
+
+    verifyToken = async (token) => {
+        let config = {
+            method: "GET",
+            headers: {
+                authorization: `Bearer ${token}`
+            }
+        }
+        fetch('https://reactcourseapi.herokuapp.com/verifytoken', config)
+            .then(res => {
+                if (res.ok) {
+                    this.setState({
+                        loading: false,
+                    })
+                }
+                else {
+                    localStorage.removeItem("token");
+                    this.props.history.push("/login")
+                }
+            })
+    }
+
+    render() {
+        const splash = (
+            <div className="full-centered">
+                <h1>Carganding...</h1>
+            </div>
+        );
+        return this.state.loading ? splash : this.props.children
+
+    }
+}
+
+export default withRouter(Splash);
